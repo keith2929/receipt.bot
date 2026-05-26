3b74eced73ceac7e4dd1c04aeb16c5faf6b815d0
keith2929 <keithtankaijun@gmail.com>
2026-05-27 01:54:31 +0800

    updated the ocr service

--- start_bot.bat
+++ start_bot.bat
@@ -1,5 +1,10 @@
 @echo off
 cd /d "%~dp0"
 call venv\Scripts\activate
+
+echo Checking requirements...
+pip install -r requirements.txt -q --disable-pip-version-check
+echo Requirements OK.
+
 python bot.py
 pause
